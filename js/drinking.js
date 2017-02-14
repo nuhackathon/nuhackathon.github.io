012859e85e11daca8966928e9a658008d6140ada
sahakorn Buangam <sahakorn.new@gmail.com>
2017-02-14 20:57:34 +0700

    Remove http
    remove http from js

--- js/drinking.js
+++ js/drinking.js
@@ -7,7 +7,7 @@ function insert(){
 	var under = $("#under").val();
 	var interview = $("#under").val();
 	var JSONObject= {"gender":gender,"name":name,"email":email ,"phone":phone, "job":job ,"under":under,"interview":interview};
-	$.post( "http://www.frontiertech.co.th:5003/regis",{infor: JSON.stringify(JSONObject)});
+	$.post( "//www.frontiertech.co.th:5003/regis",{infor: JSON.stringify(JSONObject)});
 	alert("ระบบยืนยันการลงทะเบียน กรุณารอรับการตอบรับผ่านอีเมล์");
 	
 
